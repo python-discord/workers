7eb9266d957b475f95a1366452787c91604bc299
Joe Banks <joseph@josephbanks.me>
2021-02-24 13:30:43 +0000

    Annotate github filter worker

--- github-filter-worker/src/index.ts
+++ github-filter-worker/src/index.ts
@@ -1,8 +1,10 @@
 addEventListener('fetch', (event) => {
+  // On request run our below logic.
   event.respondWith(handleRequest(event.request))
 })
 
 export async function handleRequest(request: Request): Promise<Response> {
+  // Don't apply any logic to non-POSTs.
   if (request.method !== 'POST') {
     return new Response(
       'Worker lives! Ignoring this request because it is not a POST.',
@@ -10,16 +12,23 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
 
 
+  // Clone the request so that when we read JSON we can still forward it on later.
   let json = await request.clone().json();
 
+  // Check if username is like "joe[bot]" or coveralls.
   let isCoveralls = json.sender?.login?.indexOf("coveralls") !== -1;
   let isGitHubBot = json.sender?.login?.indexOf('[bot]') !== -1;
 
+  // Combine logic.
   let botPayload = isCoveralls || isGitHubBot;
 
+  // If payload is not from a bot.
   if (!botPayload) {
+    // Create a new URL object to break out the
+
     let url = new URL(request.url)
 
+    // Check for invalid config.
     if (url.pathname === '/') {
       return new Response(
         'Make sure to specify webhook components like /:id/:token',
@@ -28,10 +37,13 @@ export async function handleRequest(request: Request): Promise<Response> {
 
     let [, id, token] = url.pathname.split('/')
 
+    // Format for a webhook
     let template = `https://discord.com/api/webhooks/${id}/${token}/github`;
 
+    // Pass on data to Discord as usual
     return await fetch(template, request)
   }
 
+  // Ignore any bot payload.
   return new Response(`Ignored by github-filter-worker`)
 }
