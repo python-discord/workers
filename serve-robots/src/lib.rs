ee8e85c7bf8f0ee43be27025b566d32abd9d6963
Joe Banks <joe@jb3.dev>
2021-12-11 09:21:24 +0000

    Pass request through to host when no robots.txt found

--- serve-robots/src/lib.rs
+++ serve-robots/src/lib.rs
@@ -1,10 +1,9 @@
-use worker::*;
 use include_dir::{include_dir, Dir};
+use worker::*;
 
 mod utils;
 
 const ROBOTS_DIR: Dir = include_dir!("robots");
-
 
 fn log_request(req: &Request) {
     console_log!(
@@ -24,19 +23,18 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
     let router = Router::new();
 
     router
-        .get("/robots.txt", |req, _| {
+        .get_async("/robots.txt", |req, _| async move {
             if let Ok(url) = req.url() {
                 if let Some(host) = url.host_str() {
                     if let Some(file) = ROBOTS_DIR.get_file(host) {
                         Response::ok(file.contents_utf8().unwrap())
                     } else {
-                        Response::error(host, 404)
+                        let req = Request::new(&String::from(url), Method::Get).unwrap();
+                        Fetch::Request(req).send().await
                     }
-
                 } else {
                     Response::error("No host provided", 404)
                 }
-
             } else {
                 Response::error("Cannot parse URL", 400)
             }
