d600d6baff9bc6738692d256a3e686ea964cfcb5
Joe Banks <joe@jb3.dev>
2023-09-05 00:52:36 +0100

    rejuvenate github-filter worker

--- github-filter/src/bindings.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-  const labels: KVNamespace
-  const HONEYCOMB_KEY: string
-}
-
-export {}
--- github-filter/src/index.ts
+++ github-filter/src/index.ts
@@ -1,125 +1,124 @@
-import { Config, hc } from '@cloudflare/workers-honeycomb-logger'
+import { Config, wrapModule } from '@cloudflare/workers-honeycomb-logger'
+import type { ExecutionContext } from "@cloudflare/workers-types"
 
 const hcConfig: Config = {
-  apiKey: HONEYCOMB_KEY,
   dataset: 'worker-discord-github-filter',
   sampleRates: {
-    '2xx': 20,
-    '3xx': 20,
-    '4xx': 5,
+    '1xx': 1,
+    '2xx': 1,
+    '3xx': 1,
+    '4xx': 1,
     '5xx': 1,
     exception: 1,
   },
 }
 
-const SHIPIT_EMOTE = '<:shipit:826492371813400637>'
+interface Env {
+  LABELS: KVNamespace
+}
 
-const listener = hc(hcConfig, (event) => {
-  event.respondWith(handleRequest(event.request))
-})
-
-addEventListener('fetch', listener)
-
-export async function handleRequest(request: Request): Promise<Response> {
-  // Don't apply any logic to non-POSTs.
-  if (request.method !== 'POST') {
-    return new Response(
-      'Worker lives! Ignoring this request because it is not a POST.',
-    )
-  }
-
-  // Clone the request so that when we read JSON we can still forward it on later.
-  let json = await request.clone().json()
-
-  request.tracer.addData({
-    githubEvent: request.headers.get('X-GitHub-Event'),
-    sender: json.sender?.login,
-  })
-
-  // Check if username is like "joe[bot]" or coveralls.
-  let isCoveralls = json.sender?.login?.indexOf('coveralls') !== -1
-  let isGitHubBot = json.sender?.login?.indexOf('[bot]') !== -1
-  let isSentry = json.sender?.login?.indexOf('sentry-io') !== -1
-  let isDependabotBranchDelete =
-    json.ref?.indexOf('dependabot') !== -1 &&
-    request.headers.get('X-GitHub-Event') === 'delete'
-  let isBotPRApprove =
-    json.pull_request?.user?.login?.indexOf('[bot]') !== -1 &&
-    request.headers.get('X-GitHub-Event') === 'pull_request_review'
-
-  let isEmptyReview =
-    json.review?.state === 'commented' &&
-    request.headers.get('X-GitHub-Event') === 'pull_request_review' &&
-    json.review?.body === null
-
-  let isBlackNonMainPush =
-    json.ref !== 'refs/heads/main' &&
-    json.repository?.name == 'black' &&
-    json.repository?.owner?.login == 'psf' &&
-    request.headers.get('X-GitHub-Event') === 'push'
-
-  // Combine logic.
-  let botPayload =
-    isCoveralls ||
-    (isGitHubBot && !isSentry) ||
-    isDependabotBranchDelete ||
-    isBotPRApprove
-  let noisyUserActions = isEmptyReview
-
-  let shouldIgnore = botPayload || noisyUserActions || isBlackNonMainPush
-
-  request.tracer.addData({ botPayload, noisyUserActions, shouldIgnore })
-
-  // If payload is not from a bot.
-  if (!shouldIgnore) {
-    // Create a new URL object to break out the
-    let url = new URL(request.url)
-
-    // Check for invalid config.
-    if (url.pathname === '/') {
+const worker = {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    // Don't apply any logic to non-POSTs.
+    if (request.method !== 'POST') {
       return new Response(
-        'Make sure to specify webhook components like /:id/:token',
-        { status: 400 },
+        'Worker lives! Ignoring this request because it is not a POST.',
       )
     }
 
-    const data: Data = {
-      body: await request.text(),
-      headers: request.headers,
-      method: request.method,
-    }
+    // Clone the request so that when we read JSON we can still forward it on later.
+    let json: Record<string, any> = await request.clone().json()
 
-    let [, id, token] = url.pathname.split('/')
-    const resp = await sendWebhook(id, token, data)
+    request.tracer.addData({
+      githubEvent: request.headers.get('X-GitHub-Event'),
+      sender: json.sender?.login,
+    })
 
-    // Send data to any extra label webhooks
-    let extraChannels = (json.issue || json.pull_request)?.labels?.map(
-      async (label: { [key: string]: string }) => {
-        let result = await sendLabelWebhook(label.name, data)
+    // Check if username is like "joe[bot]" or coveralls.
+    let isCoveralls = json.sender?.login?.indexOf('coveralls') !== -1
+    let isGitHubBot = json.sender?.login?.indexOf('[bot]') !== -1
+    let isSentry = json.sender?.login?.indexOf('sentry-io') !== -1
+    let isDependabotBranchDelete =
+      json.ref?.indexOf('dependabot') !== -1 &&
+      request.headers.get('X-GitHub-Event') === 'delete'
+    let isBotPRApprove =
+      json.pull_request?.user?.login?.indexOf('[bot]') !== -1 &&
+      request.headers.get('X-GitHub-Event') === 'pull_request_review'
 
-        if (result) {
-          return result
-        } else {
-          request.tracer.log(`Sent extra webhook to ${label.name}'s channel.`)
-        }
-      },
-    )
+    let isEmptyReview =
+      json.review?.state === 'commented' &&
+      request.headers.get('X-GitHub-Event') === 'pull_request_review' &&
+      json.review?.body === null
 
-    if (extraChannels && extraChannels.length) {
-      // Look for any failed hooks.
-      extraChannels = await Promise.all(extraChannels)
-      const response = extraChannels.reduce(
-        (result: Response | void, item: Response | void) => result || item,
+    let isBlackNonMainPush =
+      json.ref !== 'refs/heads/main' &&
+      json.repository?.name == 'black' &&
+      json.repository?.owner?.login == 'psf' &&
+      request.headers.get('X-GitHub-Event') === 'push'
+
+    // Combine logic.
+    let botPayload =
+      isCoveralls ||
+      (isGitHubBot && !isSentry) ||
+      isDependabotBranchDelete ||
+      isBotPRApprove
+    let noisyUserActions = isEmptyReview
+
+    let shouldIgnore = botPayload || noisyUserActions || isBlackNonMainPush
+
+    request.tracer.addData({ botPayload, noisyUserActions, shouldIgnore })
+
+    // If payload is not from a bot.
+    if (!shouldIgnore) {
+      // Create a new URL object to break out the
+      let url = new URL(request.url)
+
+      // Check for invalid config.
+      if (url.pathname === '/') {
+        return new Response(
+          'Make sure to specify webhook components like /:id/:token',
+          { status: 400 },
+        )
+      }
+
+      const data: Data = {
+        body: await request.text(),
+        headers: request.headers,
+        method: request.method,
+      }
+
+      let [, id, token] = url.pathname.split('/')
+      const resp = await sendWebhook(id, token, data)
+
+      // Send data to any extra label webhooks
+      let extraChannels = (json.issue || json.pull_request)?.labels?.map(
+        async (label: { [key: string]: string }) => {
+          let result = await sendLabelWebhook(label.name, data, env.LABELS)
+
+          if (result) {
+            return result
+          } else {
+            request.tracer.log(`Sent extra webhook to ${label.name}'s channel.`)
+          }
+        },
       )
 
-      if (response) return response
+      if (extraChannels && extraChannels.length) {
+        // Look for any failed hooks.
+        extraChannels = await Promise.all(extraChannels)
+        const response = extraChannels.reduce(
+          (result: Response | void, item: Response | void) => result || item,
+        )
+
+        if (response) return response
+      }
+
+      return resp
     }
 
-    return resp
-  }
-
-  // Ignore any bot payload.
-  return new Response(`Ignored by github-filter-worker`, { status: 203 })
+    // Ignore any bot payload.
+    return new Response(`Ignored by github-filter-worker`, { status: 203 })
+  },
 }
 
 interface Data {
@@ -131,8 +130,9 @@ interface Data {
 async function sendLabelWebhook(
   label: string,
   data: Data,
+  labelNamespace: KVNamespace
 ): Promise<Response | void> {
-  let channel = await labels.get(label)
+  let channel = await labelNamespace.get(label)
 
   if (channel) {
     let id, token
@@ -142,7 +142,7 @@ async function sendLabelWebhook(
     } catch {
       return new Response(
         `Could not parse label webhook channel ${channel}. ` +
-          `Make sure it's of the format /:id/:token.`,
+        `Make sure it's of the format /:id/:token.`,
         { status: 400 },
       )
     }
@@ -156,7 +156,7 @@ async function sendWebhook(id: string, token: string, data: Data) {
   let template = `https://discord.com/api/webhooks/${id}/${token}/github?wait=1`
 
   let new_request = new Request(template, {
-    body: data.body.replaceAll(':shipit:', SHIPIT_EMOTE),
+    body: data.body,
     headers: data.headers,
     method: data.method,
   })
@@ -164,3 +164,5 @@ async function sendWebhook(id: string, token: string, data: Data) {
   // Pass on data to Discord as usual
   return await fetch(template, new_request)
 }
+
+export default wrapModule(hcConfig, worker)
